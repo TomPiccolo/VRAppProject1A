6aedd1c3b44c6ecdc2d7f1dcfc46c27b34b02beb
TomPiccolo <50777275+TomPiccolo@users.noreply.github.com>
2020-11-12 16:52:53 -0500

    SuccessfulHandsBuildNoControllers
    Hand build was successful but could not switch from hands to controllers

--- VRAppProject1A/Assets/HandPresence.cs
+++ VRAppProject1A/Assets/HandPresence.cs
@@ -5,10 +5,14 @@ using UnityEngine.XR;
 
 public class HandPresence : MonoBehaviour
 {
+    public bool showController = false;
     public InputDeviceCharacteristics controllerCharacteristics;
     public List<GameObject> controllerPrefabs;
+    public GameObject handModelPrefab;
+
     private InputDevice targetDevice;
     private GameObject spawnedController;
+    private GameObject spawnedHandModel;
 
     // Start is called before the first frame update
     void Start()
@@ -35,24 +39,23 @@ public class HandPresence : MonoBehaviour
                 Debug.LogError("Did not find corresponding controller model");
                 spawnedController = Instantiate(controllerPrefabs[0], transform);
             }
+            spawnedHandModel = Instantiate(handModelPrefab, transform);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
-            Debug.Log("Pressing Primary Button");
-
-        
-        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) &&triggerValue > 0.1f)
-            Debug.Log("Trigger Pressed" + triggerValue);
-
-        
-        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero)
-            Debug.Log("Primary TouchPad" + primary2DAxisValue);
-
+        if (showController)
+        {
+            spawnedHandModel.SetActive(false);
+            spawnedController.SetActive(true);
+        }
+       else
+        {
+            spawnedHandModel.SetActive(true);
+            spawnedController.SetActive(false);
+        }
 
     }
 }
