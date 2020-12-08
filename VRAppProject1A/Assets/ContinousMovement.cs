cae4c3d903a2e31d2a9010f1fde62bcb1f385626
TomPiccolo <50777275+TomPiccolo@users.noreply.github.com>
2020-12-08 11:28:00 -0500

    Added2ndMovementCode

--- VRAppProject1A/Assets/ContinousMovement.cs
+++ VRAppProject1A/Assets/ContinousMovement.cs
@@ -8,13 +8,16 @@ public class ContinousMovement : MonoBehaviour
 {
     public float speed = 1;
     public XRNode inputSource;
+
+    private XRRig rig;
     private Vector2 inputAxis;
     private CharacterController character;
 
     // Start is called before the first frame update
     void Start()
     {
-        character = GetComponent<CharacterController>(); 
+        character = GetComponent<CharacterController>();
+        rig = GetComponent<XRRig>();
     }
 
     // Update is called once per frame
@@ -26,7 +29,8 @@ public class ContinousMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Vector3 direction = new Vector3(inputAxis.x, 0, inputAxis.y);
+        Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
         character.Move(direction * Time.fixedDeltaTime * speed);
     }
 }
