fbf4d1226f7cc4f7699c29ca68303544f258d833
TomPiccolo <50777275+TomPiccolo@users.noreply.github.com>
2020-12-08 15:31:42 -0500

    CompletedTutorial
    Completed tutorial and added snap turn to right hand

--- VRAppProject1A/Assets/ContinousMovement.cs
+++ VRAppProject1A/Assets/ContinousMovement.cs
@@ -10,6 +10,7 @@ public class ContinousMovement : MonoBehaviour
     public XRNode inputSource;
     public float gravity = -9.81f;
     public LayerMask groundLayer;
+    public float additionalHeight = 0.2f;
 
     private float fallingSpeed;
     private XRRig rig;
@@ -32,6 +33,8 @@ public class ContinousMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        CapsuleFollowHeadset();
+
         Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0);
         Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y);
         character.Move(direction * Time.fixedDeltaTime * speed);
@@ -46,6 +49,12 @@ public class ContinousMovement : MonoBehaviour
 
         fallingSpeed += gravity * Time.fixedDeltaTime;
         character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+    }
+    void CapsuleFollowHeadset()
+    {
+        character.height = rig.cameraInRigSpaceHeight + additionalHeight;
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        character.center = new Vector3(capsuleCenter.x, character.height/2 + character.skinWidth, capsuleCenter.z);
     }
     bool CheckIfGrounded()
     {
